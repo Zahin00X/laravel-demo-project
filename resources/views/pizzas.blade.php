374c0c9ba65777e0eb8f92090996bfd36d8755b5
zahin00x <zahin0013@gmail.com>
2022-05-11 12:31:58 +0600

    First Commit

---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+            <table>
+
+            
+            </table>
+            <div class="content">
+                <div class="title m-b-md">
+                    Pizza List <br>
+                </div>
+
+                <p>{{ $type }} - {{ $base }} - {{ $price }}</p>
+
+                @if($price > 15)
+                <p>Pizza is expensive</p>
+                @elseif($price < 5)
+                <p>Pizza is cheap</p>
+                @else
+                <p>This pizaa is normally priced</p>
+                @endif
+
+                @unless($base == "cheesy crust")
+                <p>You dont have a cheesy crust</p>
+                @endunless
+
+                @php 
+                    $name = "shaun";
+                    echo($name);
+                @endphp
+
+            </div>
+        </div>
+    </body>
+</html>
